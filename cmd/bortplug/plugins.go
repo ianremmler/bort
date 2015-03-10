84a779eb4ec39c3115c43d185c4eb49a423cd227
Ian Remmler <ian@remmler.org>
2015-03-10 10:34:05 -0500

    Moved plugins to their own directory.

--- cmd/bortplug/plugins.go
+++ cmd/bortplug/plugins.go
@@ -1,7 +1,7 @@
 package main
 
 import (
-	_ "github.com/ianremmler/bort/calc"
-	_ "github.com/ianremmler/bort/flip"
-	_ "github.com/ianremmler/bort/forecast"
+	_ "github.com/ianremmler/bort/plugin/calc"
+	_ "github.com/ianremmler/bort/plugin/flip"
+	_ "github.com/ianremmler/bort/plugin/forecast"
 )
