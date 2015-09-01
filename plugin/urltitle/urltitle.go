7a9d2c0240c677120995d4391d288f8e7a6d5d3f
Ian Remmler <ian@remmler.org>
2015-09-01 13:09:19 -0500

    urltitle: Add title prefix and suffix options.

--- plugin/urltitle/urltitle.go
+++ plugin/urltitle/urltitle.go
@@ -3,6 +3,7 @@
 package urltitle
 
 import (
+	"encoding/json"
 	"log"
 	"net/http"
 	"strings"
@@ -11,6 +12,16 @@ import (
 	"github.com/mvdan/xurls"
 	"golang.org/x/net/html"
 )
+
+var cfg = &Config{
+	Prefix: "",
+	Suffix: "",
+}
+
+type Config struct {
+	Prefix string
+	Suffix string
+}
 
 func findNode(node *html.Node, path ...string) *html.Node {
 	for i := range path {
@@ -44,12 +55,19 @@ func extractTitle(in, out *bort.Message) error {
 		out.Type = bort.PrivMsg
 		text := strings.TrimSpace(title.FirstChild.Data)
 		text = strings.SplitN(text+"\n", "\n", 2)[0] // first line
-		out.Text = text
+		out.Text = cfg.Prefix + text + cfg.Suffix
 	}
 	return nil
 }
 
+func setup(cfgData []byte) error {
+	return json.Unmarshal(cfgData, &struct {
+		*Config `json:"urltitle"`
+	}{cfg})
+}
+
 func init() {
+	bort.RegisterSetup(setup)
 	urlRE, err := xurls.StrictMatchingScheme("http")
 	if err != nil {
 		log.Println("urltitle: error setting regexp")
